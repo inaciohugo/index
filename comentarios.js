97fc17b66ea4b42a7a0559f1bfb1291ca7c584b0
inaciohugo <hugoinacioo@gmail.com>
2022-07-12 23:16:20 +0100

    primeiro -commit

---
+++ comentarios.js
@@ -0,0 +1,47 @@
+// Aqui eu tenho um comnetário em linha
+
+/*
+
+Aqui eu tenho um comentario multi linha
+
+O codigo abaixo escreve alguna coisa no devtools
+
+
+
+Array 
+* uma lista
+*agrupamento de dado
+*/
+console.log([
+    "Leite"
+    "Ovos"
+])
+
+
+// Tipos de dados
+
+/* Conforme o ECMASscript temos 9 tipos de dados:
+
+Data types
+- primitive / primitive value
+- strutural
+- strutural primitive
+
+## primitives
+- String "ABC" "BCB" 
+- number
+- Boolean
+- undfined
+- Symbol
+- BigInt
+
+## Estrutural primitive
+
+*Object
+- Array
+- Map
+- Set
+- Date
+
+#strutural primitive
+* null
